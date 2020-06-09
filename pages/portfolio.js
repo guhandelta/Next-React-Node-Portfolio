e140640a53ace11595a602f11dfc885581081242
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-06-09 16:02:17 -0400

    Added the feature to create dyamic pages for portfoloi detail page, using Next/Routes

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 import { BaseLayout } from '../components/layouts'
 
 
 class Portfolio extends Component {
 
+    static async getInitialProps({ query }) {
+        let post = [];
+        try {
+            const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${query.id}`);
+            post = res.data;
+        } catch (err) {
+            console.error(err);
+        }
+        return { portfolio: post };
+    }
+
     render() {
+        const { portfolio } = this.props;
         return (
             <BaseLayout>
-                <h2>Portfolio Page</h2>
+                <h2>{portfolio.title}</h2>
+                <p>{portfolio.body}</p>
             </BaseLayout>
         )
     }
--- pages/portfolios.js
+++ pages/portfolios.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import axios from 'axios'
-import Link from 'next/link'
+// import Link from 'next/link'
 import { BaseLayout } from '../components/layouts'
+import { Link } from '../routes'
+
 
 class Portfolios extends Component {
 
@@ -9,26 +11,25 @@ class Portfolios extends Component {
         let posts = [];
 
         try {
-
-            const res = await axios.get('https://jsonplaceholder.typicode.com/posts');
+            const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/`);
             posts = res.data;
         } catch (err) {
             console.error(err);
         }
-        return { posts: posts.slice(0, 10) };
+        return { portfolio: posts.slice(0, 10) };
     }
 
     render() {
-        const { posts } = this.props;
+        const { portfolio } = this.props;
         return (
             <BaseLayout>
                 <h1>Portfolios Page</h1>
                 <ul>
                     {
-                        posts.map(post => {
+                        portfolio.map(post => {
                             return (
                                 <li key={post.id}>
-                                    <Link as={`portfolios/${post.id}`} href={`portfolios/[id]`}>
+                                    <Link route={`portfolios/${post.id}`}>
                                         <a>
                                             {post.title}
                                         </a>
