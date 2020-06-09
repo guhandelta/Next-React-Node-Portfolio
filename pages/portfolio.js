eaa2acd3011c696f288c20645f66da858ae30ef9
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-06-09 15:05:26 -0400

    Created custom server using Express

---
+++ pages/portfolio.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { BaseLayout } from '../components/layouts'
+
+
+class Portfolio extends Component {
+
+    render() {
+        return (
+            <BaseLayout>
+                <h2>Portfolio Page</h2>
+            </BaseLayout>
+        )
+    }
+}
+
+export default Portfolio;
