f2b33abe7528f02a63620f8454d251cca5149c50
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-06-09 23:50:24 -0400

    Created a basic Page Header, using a React class component

--- components/shared/Header.js
+++ components/shared/Header.js
@@ -1,37 +1,63 @@
-import React, { Component } from 'react'
+import { Component } from 'react';
 import Link from 'next/link'
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem
+} from 'reactstrap';
 
-class Header extends Component {
+const BSNavLink = props => {
+    const { title, href } = props;
+    return (
+        <Link href={href}>
+            <a className="nav-link">
+                {title}
+            </a>
+        </Link>
+    );
+}
+
+
+export default class Header extends Component {
+    state = { isOpen: false }
+    toggle = () => this.setState({ isOpen: !this.state.isOpen })
     render() {
         return (
-            <React.Fragment>
-                <Link href="/">
-                    <a>
-                        Home
-                    </a>
-                </Link>&emsp;
-                <Link href="/about">
-                    <a>
-                        About
-                    </a>
-                </Link>&emsp;
-                <Link href="/portfolios">
-                    <a>
-                        Portfolios
-                    </a>
-                </Link>&emsp;
-                <Link href="/blogs">
-                    <a>
-                        Blogs
-                    </a>
-                </Link>&emsp;
-                <Link href="/cv">
-                    <a>
-                        Cv
-                    </a>
-                </Link>&emsp;
-            </React.Fragment>
-        );
+            <div>
+                <Navbar color="light" light expand="md">
+                    <NavbarBrand>
+                        <Link href="/">
+                            <a className='nav-link name'>
+                                Guhaprasaanth
+                            </a>
+                        </Link>
+                    </NavbarBrand>
+                    <NavbarToggler onClick={this.toggle} />
+                    <Collapse isOpen={this.isOpen} navbar>
+                        <Nav className="mr-auto" navbar>
+                            <NavItem>
+                                <BSNavLink title="Home" href="/" />
+                            </NavItem>&emsp;&nbsp;
+                            <NavItem>
+                                <BSNavLink title="About" href="/about" />
+                            </NavItem>&emsp;&nbsp;
+                            <NavItem>
+                                <BSNavLink title="Portfolios" href="/portfolios" />
+                            </NavItem>&emsp;&nbsp;
+                            <NavItem>
+                                <BSNavLink title="Blogs" href="/blogs" />
+                            </NavItem>&emsp;&nbsp;
+                            <NavItem>
+                                <BSNavLink title="CV" href="/cv" />
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
+                </Navbar>
+            </div>
+        )
     }
 }
-export default Header;
+
