242f69937080df0f38c3e92e774195f187d98975
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-07-18 17:07:49 -0400

    Abstracted all the Auth0 related information using Environment Variables

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const path = require('path');
+const DotEnv = require('dotenv-webpack');
 
 
 module.exports = {
     webpack: config => { //Recieve config obj as param
         config.resolve.alias['@'] = path.resolve(__dirname);   // allows replacing ../../ with @ || @ -> point to the root folder
+        config.plugins.push(new DotEnv({ silent: true })); // supress all warnings
         return config;
     }
 }
