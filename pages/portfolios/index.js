27a18e1745d805f1e8fc5e3c927008dd0c417a80
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-08-05 18:13:05 -0400

    Added the funcitonality to fetch the portfolio details from the node server API

--- pages/portfolios/index.js
+++ pages/portfolios/index.js
@@ -1,23 +1,17 @@
 import Link from 'next/link'
 import { BaseLayout } from '@/components/layouts'
 import { BasePage } from '@/components/'
-import { useGetPosts } from '@/actions'
 import { useGetUser } from '@/actions/user'
+import PortfolioApi from '@/lib/api/portfolios'
 
-const Portfolios = () => {
-
-    const { data, error, loading } = useGetPosts();
+const Portfolios = ({ portfolios }) => {
     const { data: userData, loading: userLoading } = useGetUser();
-    // useSWR will execute the given fn() to fetch the data and assign the fetch result to teh data prop, depending on the result
-    debugger;
-    // onPageLoad => useGetPosts() returns an [] on the 1st call and after page is rendered, useEffect gets triggered and -
-    //- sets the fetched data to the state, which will rerender the component => displaying the fetched data thru Portfolios()
-    const renderPosts = (posts) => {
-        return posts.map(post =>
-            <li key={post.id} style={{ 'fontSize': '20px' }}>
-                <Link as={`/portfolios/${post.id}`} href="/portfolios/[id]">
+    const renderPortfolios = (portfolios) => {
+        return portfolios.map(portfolio =>
+            <li key={portfolio._id} style={{ 'fontSize': '20px' }}>
+                <Link as={`/portfolios/${portfolio._id}`} href="/portfolios/[id]">
                     <a>
-                        {post.title}
+                        {portfolio.title}
                     </a>
                 </Link>
             </li>
@@ -27,23 +21,21 @@ const Portfolios = () => {
     return (
         <BaseLayout user={userData} loading={userLoading} >
             <BasePage>
-                <h1>Portfolios Page</h1>
-                {loading &&
-                    <h5>Loading Data.....</h5>
-                }
-                {data &&
-                    <ul>
-                        {renderPosts(data)}
-                    </ul>
-                }
-                {error &&
-                    <div className="alert alert-danger">
-                        {error.message}
-                    </div>
-                }
+                <ul>
+                    {renderPortfolios(portfolios)}
+                </ul>
             </BasePage>
         </BaseLayout>
     )
+}
+
+// This fn() will be called during the build time - This fn() helps boost performace by building static pages with dynamic data
+export async function getStaticProps() {
+    const json = await new PortfolioApi().getAll();
+    const portfolios = json.data; //Will get the portfolios for this page, as props
+    return {
+        props: { portfolios }
+    }
 }
 
 export default Portfolios;
