0855a2f673b2438b614e79d3f4ae1321470da0df
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-08-21 20:16:58 -0400

    Enabled Portfolio creation by providing and assigning the access token to the request header

--- pages/api/v1/portfolios.js
+++ pages/api/v1/portfolios.js
@@ -5,10 +5,9 @@ export default async function createPortfolio(req, res) {
 
     try {
         const { accessToken } = await auth0.getSession(req);
-        console.log(accessToken);
-        await new PortfoliosApi().createPortfolio(req.body);
-        // await new PortfoliosApi(accessToken).createPortfolio(req.body);
-        return res.json({ message: 'Porfolio created successfully....' });
+        //This request takes place from the Node.js env, not from browser env
+        const newPortfolio = await new PortfoliosApi(accessToken).createPortfolio(req.body);
+        return res.json(newPortfolio.data);
     } catch (e) {
         console.log('Api Error!!......');
         return res.status(e.status || 400).end(e.message);
