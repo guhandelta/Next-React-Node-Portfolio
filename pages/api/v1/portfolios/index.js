aa0922fa033cb33a8598cce200319f4708ca9f9e
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-09-05 16:28:17 -0400

    Reduced code redndancy by combining the common functionalities of the API endpoints into a single parent class, leaving only the API specific funcitonalities in the separate files

--- pages/api/v1/portfolios/index.js
+++ pages/api/v1/portfolios/index.js
@@ -7,7 +7,7 @@ export default async function createPortfolio(req, res) {
         const { accessToken } = await auth0.getSession(req);
         console.log("AccessToken", accessToken);
         //This request takes place from the Node.js env, not from browser env
-        const newPortfolio = await new PortfoliosApi(accessToken).createPortfolio(req.body);
+        const newPortfolio = await new PortfoliosApi(accessToken).create(req.body);
         return res.json(newPortfolio.data);
     } catch (e) {
         console.log('Api Error!!......');
