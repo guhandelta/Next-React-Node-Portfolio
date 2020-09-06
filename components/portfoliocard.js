ce4095b3f757eada3d6c7f2a90e49354182bb4d4
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-09-06 09:58:41 -0400

    Added a simple functionality to shorten the description text on PortfolioCard, to display PortfolioCard in uniform size

--- components/portfoliocard.js
+++ components/portfoliocard.js
@@ -1,12 +1,19 @@
 import { Card, CardHeader, CardBody, CardText, CardTitle } from 'reactstrap';
 
+const gist = (text, maxLength) => {
+    if (!text) { return ''; }
+    if (text.length <= maxLength) { return text; }
+
+    return text.substr(0, maxLength) + '...';
+}
+
 const PortfolioCard = ({ portfolio, children }) =>
     <Card className="portfolio-card">
         <CardHeader className="portfolio-card-header">{portfolio.jobTitle}</CardHeader>
         <CardBody>
             <p className="portfolio-card-city">{portfolio.location}</p>
             <CardTitle className="portfolio-card-title">{portfolio.title}</CardTitle>
-            <CardText className="portfolio-card-text">{portfolio.description}</CardText>
+            <CardText className="portfolio-card-text">{gist(portfolio.description, 125)}</CardText>
             {children}
         </CardBody>
     </Card>
