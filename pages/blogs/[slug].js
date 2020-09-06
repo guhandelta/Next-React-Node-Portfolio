e9da53d5300e4ec4daad6060d45410607a4fbf34
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-09-06 09:41:47 -0400

    Fetched and displayed the blog data on the pre-rendered Blog Details page, using getStaticProps(), with some appropriate stylings added

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,16 +1,36 @@
+import { Row, Col } from 'reactstrap'
+import { SlateView } from 'slate-simple-editor'
 import { BaseLayout, BasePage } from 'components'
 import { useGetUser } from 'actions/user'
+import BlogsApi from 'lib/api/blogs'
 
-const BlogDetail = () => {
+const BlogDetail = ({ blog }) => {
     const { data, loading } = useGetUser();
     debugger;
     return (
         <BaseLayout user={data} loading={loading} >
-            <BasePage>
-                <h1 className="my-name">Blog Detail Page</h1>
-                <h2>Some detailed stuff about the blog</h2>
+            <BasePage className="slate-container">
+                <Row>
+                    <Col md={{ size: 8, offset: 2 }}> {/*Entire page, which is divided into 12 columns, will now be divided into 6 col ||
+                    offset 2 will add 2 col padding on both sides*/}
+                        <SlateView initialContent={blog.content} />
+                    </Col>
+                </Row>
             </BasePage>
         </BaseLayout>
     )
 }
+
+export async function getStaticPaths() {
+    const blogsJson = await new BlogsApi().getAll();
+    const blogs = blogsJson.data;
+    const paths = blogs.map(blog => ({ params: { slug: blog.slug } })); // Object with params : slug
+    return { paths, fallback: false }; //fallback: false => resolve to 404 error
+}
+
+export async function getStaticProps({ params }) {
+    const blogJson = await new BlogsApi().getBySlug(params.slug);
+    return { props: { blog: blogJson.data } };
+}
+
 export default BlogDetail;
