0c253e5ac89370a596b6cd0b7bbff58ef3de897e
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-09-06 17:56:08 -0400

    Added content to the about page, with appropriate styling

--- pages/About.js
+++ pages/About.js
@@ -1,18 +1,53 @@
-import React, { Component } from 'react'
-import { BaseLayout } from '@/components/layouts'
-import { BasePage } from '@/components/'
-import { useGetUser } from '@/actions/user'
+import { useEffect } from 'react';
+import { Row, Col } from 'reactstrap';
+import { BaseLayout, BasePage } from 'components';
+import { useGetUser } from 'actions/user';
+
 
 const About = () => {
     const { data, loading } = useGetUser();
-    debugger;
+
+    useEffect(() => { //
+        return () => {
+            window.__isAboutLoaded = true; // To say, that the page was already visited and not to execute the animation again
+        }
+    })
+
+    const createFadeInClass = () => {
+        if (typeof window !== 'undefined') { //Checking it app is viewed in a browser
+            return window.__isAboutLoaded ? '' : 'fadein'; //Checking if thisis the first visit, to add the `fadein` class, for the animaiton
+        }
+
+        return 'fadein';
+    }
+
     return (
-        <BaseLayout user={data} loading={loading} >
-            <BasePage>
-                <h1 className="my-name">About Page</h1>
-                <h2>Some random stuff about me</h2>
+        <BaseLayout user={data} loading={loading}>
+            <BasePage className="about-page">
+                <Row className="mt-5">
+                    <Col md="6">
+                        <div className="left-side">
+                            <h1 className={`title ${createFadeInClass()}`}>Hello, Welcome</h1>
+                            <h4 className={`subtitle ${createFadeInClass()}`}>To About Page</h4>
+                            <p className={`subsubTitle ${createFadeInClass()}`}>Feel free to get to know me.</p>
+                        </div>
+                    </Col>
+                    <Col md="6">
+                        <div className={`${createFadeInClass()}`}>
+                            <p>I am Guhaprasaanth, I am an experienced software engineer and freelance developer. </p>
+                            <p>
+                                I have a Master's degree in Computer Science and experience in  working
+                                on a wide range of technologies and projects.
+                            </p>
+                            <p>
+                                Versatile Front-end/Web Developer with experience designing, developing, and managing complex responsive websites and web applications using JavaScript libraries and frameworks like ReactJS & NodeJS and Python frameworks like Django and Flask.
+                         </p>
+                        </div>
+                    </Col>
+                </Row>
             </BasePage>
         </BaseLayout>
     )
 }
+
 export default About;
