8463bf411bc13781584c83e3df76803e2cb1c385
Guhaprasaanth Nandagopal <guhan.delta@gmail.com>
2020-09-07 18:26:31 -0400

    Added <Head> title to the index page for SEO

--- components/BasePage.js
+++ components/BasePage.js
@@ -3,18 +3,20 @@ import { Container } from 'reactstrap' // Wrapping the component using this, wil
 
 const BasePage = props => {
     const {
+        indexPage,
         className = '',
         header,
         children,
         title = "Portfolio - Guhaprasaanth"
     } = props;
+    const pageType = indexPage ? 'index-page' : 'base-page';
     return (
         <>
             <Head>
                 <meta name="viewport" content="initial-scale=1.0, width=device-width" />
                 <title>{title}</title>
             </Head>
-            <div className={`base-page ${className}`}>
+            <div className={`${pageType} ${className}`}>
                 <Container>
                     {header &&
                         <div className="page-header">
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react'
 import Typed from 'react-typed'
 import { Container, Row, Col } from 'reactstrap';
-import { BaseLayout } from 'components/layouts/';
+import { BaseLayout, BasePage } from 'components';
 import { useGetUser } from 'actions/user'
 
 const ROLES = ['Full Stack Developer', 'ReactJS', 'Flask', 'Blockchain Enthusiast', 'Nature Enthusiast', 'Team Player', 'NodeJS', 'Django'];
@@ -32,70 +32,75 @@ const Index = () => {
       className={`cover ${isFlipping ? 'cover-orange' : 'cover-blue'}`}
       navClass="transparent"
     >
-      <div className="main-section">
-        <div className="background-image">
-          <img src="/images/background-index-tamil.png" />
-        </div>
-        <Container>
-          <Row>
-            <Col md="6">
-              <div className="hero-section">
-                <div className={`flipper ${isFlipping ? 'isFlipping' : ''}`}>
-                  <div className="front">
-                    <div className="hero-section-content">
-                      <h2> Full Stack Web Developer </h2>
-                      <div className="hero-section-content-intro">
-                        Have a look at my portfolio and job history.
+      <BasePage
+        title="Portfolio - Guhaprasaanth"
+        indexPage
+      >
+        <div className="main-section">
+          <div className="background-image">
+            <img src="/images/background-index-tamil.png" />
+          </div>
+          <Container>
+            <Row>
+              <Col md="6">
+                <div className="hero-section">
+                  <div className={`flipper ${isFlipping ? 'isFlipping' : ''}`}>
+                    <div className="front">
+                      <div className="hero-section-content">
+                        <h2> Full Stack Web Developer </h2>
+                        <div className="hero-section-content-intro">
+                          Have a look at my portfolio and job history.
                         </div>
+                      </div>
+                      <img className="image" src="/images/section-1.png" />
+                      <div className="shadow-custom">
+                        <div className="shadow-inner"> </div>
+                      </div>
                     </div>
-                    <img className="image" src="/images/section-1.png" />
-                    <div className="shadow-custom">
-                      <div className="shadow-inner"> </div>
-                    </div>
-                  </div>
-                  <div className="back">
-                    <div className="hero-section-content">
-                      <h2> Full Stack Web Developer </h2>
-                      <div className="hero-section-content-intro">
-                        Have a look at my portfolio and job history.
+                    <div className="back">
+                      <div className="hero-section-content">
+                        <h2> Full Stack Web Developer </h2>
+                        <div className="hero-section-content-intro">
+                          Have a look at my portfolio and job history.
                         </div>
-                    </div>
-                    <img className="image" src="/images/section-2.png" />
-                    <div className="shadow-custom shadow-custom-orange">
-                      <div className="shadow-inner"> </div>
+                      </div>
+                      <img className="image" src="/images/section-2.png" />
+                      <div className="shadow-custom shadow-custom-orange">
+                        <div className="shadow-inner"> </div>
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            </Col>
-            <Col md="6" className="hero-welcome-wrapper">
-              <div className="hero-welcome-text">
-                <h1>
-                  Welcome to my portfolio website.
-                  Get informed, collaborate and discover projects I was working on through the years!
+              </Col>
+              <Col md="6" className="hero-welcome-wrapper">
+                <div className="hero-welcome-text">
+                  <h1>
+                    Welcome to my portfolio website.
+                    Get informed, collaborate and discover projects I was working on through the years!
                   </h1>
-              </div>
-              <Typed
-                loop
-                strings={ROLES}
-                typeSpeed={70}
-                backSpeed={70}
-                backDelay={1000}
-                loopCount={0}
-                showCursor
-                className="self-typed"
-                cursorChar="|"
-              >
-              </Typed>
-              <div className="hero-welcome-bio">
-                <h1>
-                  Let's take a look on my work.
+                </div>
+                <Typed
+                  loop
+                  strings={ROLES}
+                  typeSpeed={70}
+                  backSpeed={70}
+                  backDelay={1000}
+                  loopCount={0}
+                  showCursor
+                  className="self-typed"
+                  cursorChar="|"
+                >
+                </Typed>
+                <div className="hero-welcome-bio">
+                  <h1>
+                    Let's take a look on my work.
                   </h1>
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </div>
+      </BasePage>
     </BaseLayout >
   )
 }
